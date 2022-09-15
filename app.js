78dc7f017969d0b071b8330958a0570fef36efe1
DragiboO <dragibo.scotch@gmail.com>
2022-09-15 14:43:20 +0200

    :sparkles: Random color

--- app.js
+++ app.js
@@ -7,17 +7,9 @@ function click() {
         count += 1
         btn.value = count
 
-        if (count % 3 == 0) {
-            btn.style.backgroundColor = "green"
-        } else if (count % 7 == 0) {
-            btn.style.backgroundColor = "blue"
-        } else if (count % 5 == 0) {
-            btn.style.backgroundColor = "grey"
-        } else if (count % 11 == 0) {
-            btn.style.backgroundColor = "red"
-        } else {
-            btn.style.backgroundColor = "white"
-        }
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+        btn.style.backgroundColor = "#" + randomColor
     })
 }
 
